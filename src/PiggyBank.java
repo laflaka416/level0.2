7b8d7c217be730f5bfe84d8ac76c05a4c22012bb
laflaka416 <league@Mac-mini-3.local>
2016-12-10 11:33:40 -0800

    12/10/2016

--- src/PiggyBank.java
+++ src/PiggyBank.java
@@ -16,5 +16,6 @@ public static void main(String[] args) {
    	int valueNumPennies = numPennies;
    	int answer = valueNumQuarters + valueNumDimes + valueNumNickels + valueNumPennies; 
     JOptionPane.showMessageDialog(null, "you have"+ answer + "cents");
+    
 }
 }
