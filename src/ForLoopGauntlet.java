0328fd95f92f2c3ef23069c1dc38ae5ad2849f98
laflaka416 <league@WorthyMini-3.local>
2017-06-03 11:37:35 -0700

    i finally came to class lol

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,43 @@
+
+public class ForLoopGauntlet {
+	public static void main(String[] args) {
+
+		// for(int i=1;i <= 100; i++){
+		// System.out.println(i);
+		// }
+		// for(int i=100; i>=0;i--){
+		// System.out.println(i);
+		// }
+		// for(int i = 2; i<= 100; i++){
+		// if(i % 2 == 0){
+		// System.out.println(i);
+		// }
+		// }
+
+		// for(int i = 1;i<= 99;i ++){
+		// if(i % 2 == 1){
+		// System.out.println(i);
+		// }
+		// }
+
+		// for(int i = 1; i <= 500; i++){
+		// if(i % 2 == 1){
+		// System.out.println(i + "odd");
+		// }
+		// else if(i % 2 == 0){
+		// System.out.println(i + "even");
+		// }
+		// }
+
+		//for (int i = 0; i <= 777; i++) {
+		//	if (i % 7 == 0) {
+		//		System.out.println(i);
+		//	}
+		//}
+		int j = 1; 
+		for(int i = 2002;i <= 2017; i++, j++){
+      System.out.println("in " + i + ",i was " + j + " years old.");
+		  
+		}
+	}
+}
